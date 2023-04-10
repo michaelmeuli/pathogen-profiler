5a6bb1787ab2ce6d1d58541ce663be3af4ba12ab
Michael Meuli <michael.meuli@gmail.com>
2023-04-10 14:52:37 +0200

    don't keep_variant_types synonymous

--- pathogenprofiler/profiler.py
+++ pathogenprofiler/profiler.py
@@ -32,7 +32,7 @@ def bam_profiler(conf, bam_file, prefix, platform, caller, threads=1, no_flagsta
         vcf_obj = vcf_obj.add_annotations(conf["ref"],bam_obj.bam_file)
     else:
         ann_vcf_obj = vcf_obj.run_snpeff(conf["snpEff_db"],conf["ref"],conf["gff"],rename_chroms= conf.get("chromosome_conversion",None))
-    ann = ann_vcf_obj.load_ann(bed_file=conf["bed"],keep_variant_types = ["upstream","synonymous","noncoding"],min_af=min_af)
+    ann = ann_vcf_obj.load_ann(bed_file=conf["bed"],keep_variant_types = ["upstream","noncoding"],min_af=min_af)
 
 
     ### Get % and num reads mapping ###
